7ac7e7052907236b9235a15bc4a027005e9a02ff
ricardoschullerSL <rschuller@scottlogic.com>
2017-09-15 15:09:13 +0100

    Mocked out EncryptionManager from TPPManagerTests

--- src/test/java/atmbranchfinderspring/resourceserver/TPPManagerTests.java
+++ src/test/java/atmbranchfinderspring/resourceserver/TPPManagerTests.java
@@ -2,6 +2,7 @@ package atmbranchfinderspring.resourceserver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import atmbranchfinderspring.resourceserver.authentication.AuthManager;
+import atmbranchfinderspring.resourceserver.authentication.EncryptionManager;
 import atmbranchfinderspring.resourceserver.authentication.TPPManager;
 import atmbranchfinderspring.resourceserver.models.ClientCredentials;
 import atmbranchfinderspring.resourceserver.models.TPPClient;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URI;
@@ -19,6 +21,9 @@ import java.net.URISyntaxException;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TPPManagerTests {
+
+	@MockBean
+	EncryptionManager encryptionManager;
 
 	@Autowired
 	private TPPManager tppManager;
@@ -32,12 +37,11 @@ public class TPPManagerTests {
 	@Test
 	public void registerTPPClientTest() {
 		try {
-			DecodedJWT decodedJWT = authManager.getJWTVerifier().verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzb2Z0d2FyZV9pZCI6IlNjb3R0eSIsImF1ZCI6IlNjb3R0IExvZ2ljIEJhbmsiLCJzb2Z0d2FyZV9zdGF0ZW1lbnQiOiJ0ZXN0c29mdHdhcmVzdGF0ZW1lbnQiLCJpc3MiOiJPcGVuIEJhbmtpbmciLCJyZWRpcmVjdF91cmkiOiJodHRwOi8vbG9jYWxob3N0OjgwODEvcmVkaXJlY3QiLCJqdGkiOiJqd3RJZCJ9.MEUCIQD5O-L-2hIzIVZ4BlB94AN-zObshw9cHhOhrM3uAy89QgIgaSMEUTKgD9ztYOj6SgQ-vYLIGH4RK6o7NSEPSeqC-yI");
 			ClientCredentials credentials = new ClientCredentials("clientId", "clientSecret");
 			TPPClient tppClient = new TPPClient.TPPClientBuilder()
 					.setClientCredentials(credentials)
 					.setRedirectUri(new URI("http://test.com/redirect"))
-					.setSSA(decodedJWT).build();
+					.setSSA(null).build();
 
 			tppManager.registerClient(tppClient);
 			assertThat(tppClientRepository.getAllIds().size()).isEqualTo(1);
