6bb6d7f4293618dcc18545d80acff3319653de4c
ricardoschullerSL <rschuller@scottlogic.com>
2017-09-15 15:04:15 +0100

    Extraced EncryptionManager from AuthManager and mocked it out in test.

--- src/main/java/atmbranchfinderspring/resourceserver/authentication/AuthManager.java
+++ src/main/java/atmbranchfinderspring/resourceserver/authentication/AuthManager.java
@@ -3,23 +3,9 @@ package atmbranchfinderspring.resourceserver.authentication;
 import atmbranchfinderspring.resourceserver.models.TPPClient;
 import atmbranchfinderspring.resourceserver.repos.AccessTokenRepository;
 import atmbranchfinderspring.resourceserver.repos.TPPClientRepository;
-import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.security.KeyPair;
-import java.security.Security;
-import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.ECPublicKey;
 
 /**
  * @author Ricardo Schuller
@@ -31,84 +17,22 @@ import java.security.interfaces.ECPublicKey;
 @Component
 public class AuthManager implements AuthenticationManager {
 
-    private PEMParser pemParser;
-    private PEMKeyPair pemKeyPair;
-    private JcaPEMKeyConverter converter;
-    private KeyPair keyPair;
-    private ECPrivateKey privateKey;
-    private ECPublicKey publicKey;
-    private Algorithm algorithm;
-    private JWTVerifier jwtVerifier;
+
     private AccessTokenRepository accessTokenRepository;
     private TPPClientRepository tppClientRepository;
+    private EncryptionManager encryptionManager;
 
     @Autowired
-    public AuthManager(AccessTokenRepository accessTokenRepository, TPPClientRepository tppClientRepository) {
-        try {
-            this.tppClientRepository = tppClientRepository;
-            this.accessTokenRepository = accessTokenRepository;
-            Security.addProvider(new BouncyCastleProvider());
-            pemParser = new PEMParser(new InputStreamReader(getClass().getResourceAsStream("/ec256-key-pair.pem")));
-            pemKeyPair = (PEMKeyPair) pemParser.readObject();
-            converter = new JcaPEMKeyConverter();
-            keyPair = converter.getKeyPair(pemKeyPair);
-            pemParser.close();
+    public AuthManager(AccessTokenRepository accessTokenRepository, TPPClientRepository tppClientRepository, EncryptionManager encryptionManager) {
 
-            privateKey = (ECPrivateKey) keyPair.getPrivate();
-            publicKey = (ECPublicKey) keyPair.getPublic();
-
-
-            algorithm = Algorithm.ECDSA256(publicKey,privateKey);
-
-            jwtVerifier = JWT.require(algorithm)
-                    .withIssuer("Open Banking")
-                    .build();
-
-            // Use this part to print out an encoded JWT for registering client.
-//            String testJWT = JWT.create().withIssuer("Open Banking")
-//                    .withClaim("software_id","Scotty")
-//                    .withClaim("aud","Scott Logic Bank")
-//                    .withClaim("redirect_uri","http://localhost:8081/redirect")
-//                    .withClaim("software_statement","testsoftwarestatement")
-//                    .withJWTId("jwtId")
-//                    .sign(algorithm);
-//            System.out.println(testJWT);
-        } catch (IOException e) {
-            System.out.println(e);
-
-        }
+        this.tppClientRepository = tppClientRepository;
+        this.accessTokenRepository = accessTokenRepository;
+		this.encryptionManager = encryptionManager;
     }
 
-    public PEMParser getPemParser() {
-        return pemParser;
-    }
-
-    public PEMKeyPair getPemKeyPair() {
-        return pemKeyPair;
-    }
-
-    public JcaPEMKeyConverter getConverter() {
-        return converter;
-    }
-
-    public KeyPair getKeyPair() {
-        return keyPair;
-    }
-
-    public ECPrivateKey getPrivateKey() {
-        return privateKey;
-    }
-
-    public ECPublicKey getPublicKey() {
-        return publicKey;
-    }
-
-    public Algorithm getAlgorithm() {
-        return algorithm;
-    }
 
     public JWTVerifier getJWTVerifier() {
-        return jwtVerifier;
+        return encryptionManager.getJwtVerifier();
     }
 
     public Boolean isAccessTokenValid(String token) {
--- src/test/java/atmbranchfinderspring/resourceserver/AuthManagerTests.java
+++ src/test/java/atmbranchfinderspring/resourceserver/AuthManagerTests.java
@@ -1,25 +1,30 @@
 package atmbranchfinderspring.resourceserver;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import atmbranchfinderspring.resourceserver.authentication.AuthManager;
+import atmbranchfinderspring.resourceserver.authentication.EncryptionManager;
+import atmbranchfinderspring.resourceserver.models.AccessToken;
 import atmbranchfinderspring.resourceserver.models.ClientCredentials;
 import atmbranchfinderspring.resourceserver.models.TPPClient;
-import atmbranchfinderspring.resourceserver.models.AccessToken;
 import atmbranchfinderspring.resourceserver.repos.AccessTokenRepository;
 import atmbranchfinderspring.resourceserver.repos.TPPClientRepository;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthManagerTests {
+
+	@MockBean
+	EncryptionManager encryptionManager;
 
 	@Autowired
 	AuthManager authManager;
@@ -27,6 +32,7 @@ public class AuthManagerTests {
 	AccessTokenRepository accessTokenRepository;
 	@Autowired
 	TPPClientRepository tppClientRepository;
+
 
 	@Test
 	public void accessTokenCheckerTest() {
@@ -41,12 +47,11 @@ public class AuthManagerTests {
 	@Test
 	public void credentialCheckerTest() {
 		try {
-			DecodedJWT decodedJWT = authManager.getJWTVerifier().verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzb2Z0d2FyZV9pZCI6IlNjb3R0eSIsImF1ZCI6IlNjb3R0IExvZ2ljIEJhbmsiLCJzb2Z0d2FyZV9zdGF0ZW1lbnQiOiJ0ZXN0c29mdHdhcmVzdGF0ZW1lbnQiLCJpc3MiOiJPcGVuIEJhbmtpbmciLCJyZWRpcmVjdF91cmkiOiJodHRwOi8vbG9jYWxob3N0OjgwODEvcmVkaXJlY3QiLCJqdGkiOiJqd3RJZCJ9.MEUCIQD5O-L-2hIzIVZ4BlB94AN-zObshw9cHhOhrM3uAy89QgIgaSMEUTKgD9ztYOj6SgQ-vYLIGH4RK6o7NSEPSeqC-yI");
 			ClientCredentials credentials = new ClientCredentials("clientId", "clientSecret");
 			TPPClient tppClient = new TPPClient.TPPClientBuilder()
 					.setClientCredentials(credentials)
 					.setRedirectUri(new URI("http://test.com/redirect"))
-					.setSSA(decodedJWT).build();
+					.setSSA(null).build();
 			tppClientRepository.add(tppClient);
 
 			assertThat(authManager.areCredentialsCorrect(credentials.getClientId(),credentials.getClientSecret())).isEqualTo(true);
