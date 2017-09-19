fcc8d682e7156ca9e29baa89fa152a8624cea4e9
ricardoschullerSL <rschuller@scottlogic.com>
2017-09-19 08:00:16 +0100

    Expanded AuthManagerTests in multiple tests.

--- src/test/java/atmbranchfinderspring/resourceserver/AuthManagerTests.java
+++ src/test/java/atmbranchfinderspring/resourceserver/AuthManagerTests.java
@@ -9,6 +9,7 @@ import atmbranchfinderspring.resourceserver.models.TPPClient;
 import atmbranchfinderspring.resourceserver.repos.AccessTokenRepository;
 import atmbranchfinderspring.resourceserver.repos.TPPClientRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,12 +37,27 @@ public class AuthManagerTests {
 
 
 	@Test
+	@DisplayName("Check if AuthManager validates access token")
 	public void accessTokenCheckerTest() {
 		AccessToken testToken = new AccessToken("Bearer", "abc");
 		accessTokenRepository.add(testToken);
-
-		assertThat(accessTokenRepository.getAllIds().size()).isEqualTo(1);
 		assertThat(authManager.isAccessTokenValid(testToken.getAccessToken())).isEqualTo(true);
+	}
+
+	@Test
+	@DisplayName("Check if AuthManager validates access token")
+	public void addAccessTokenToRepositoryTest() {
+		AccessToken testToken = new AccessToken("Bearer", "abc");
+		accessTokenRepository.add(testToken);
+		assertThat(accessTokenRepository.getAllIds().size()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("Check if access token repository deletes access token after use")
+	public void accessTokenDeletionTest() {
+		AccessToken testToken = new AccessToken("Bearer", "abc");
+		accessTokenRepository.add(testToken);
+		authManager.isAccessTokenValid(testToken.getAccessToken());
 		assertThat(accessTokenRepository.getAllIds().size()).isEqualTo(0);
 	}
 
