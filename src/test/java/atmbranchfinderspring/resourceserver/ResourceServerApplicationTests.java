f5d4592d78e020bd45e2bba5abdb4eb6c066b451
ricardoschullerSL <rschuller@scottlogic.com>
2017-10-24 10:07:26 +0100

    Now it's really commented out.

--- src/test/java/atmbranchfinderspring/resourceserver/ResourceServerApplicationTests.java
+++ src/test/java/atmbranchfinderspring/resourceserver/ResourceServerApplicationTests.java
@@ -13,22 +13,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ResourceServerApplicationTests {
-
-	@LocalServerPort
-	private int port;
-
-	@MockBean
-	private PEMManagerImpl pemManager;
-
-	@Autowired
-	private TPPController tppController;
-// TODO: This test needs to be redone for Junit5
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class ResourceServerApplicationTests {
+//
+//	@LocalServerPort
+//	private int port;
+//
+//	@MockBean
+//	private PEMManagerImpl pemManager;
+//
+//	@Autowired
+//	private TPPController tppController;
+//  TODO: This test needs to be redone for Junit5
 //	@Test
 //	public void contextLoads() throws Exception {
 //		assertThat(tppController).isNotNull();
 //	}
-
-}
+//
+//}
